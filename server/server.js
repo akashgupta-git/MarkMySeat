e18f7accfe8f3edb86eed607078dd626a90bad95
Akash Gupta <akashgupta.20044@gmail.com>
2025-06-28 14:39:43 +0530

    Commit PORT change to 8080

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 });
 
   // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
