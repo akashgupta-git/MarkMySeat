58332862e285b96c31e1a6f27b20b8a88d9f512d
Akash Gupta <akashgupta.20044@gmail.com>
2025-11-08 00:53:20 +0530

    Commit updated frontend

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a base Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // or your backend URL
+  baseURL: "/api", // or your backend URL
 });
 
 // Automatically attach token from localStorage (or any secure storage you're using)
